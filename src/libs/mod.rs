ce06ceee092c0838a9ed37b2fcfdd7e3a63f6a5e
kodelint <kodelint@example.com>
2025-07-12 00:07:39 -0700

    Register all the new modules

---
+++ src/libs/mod.rs
@@ -0,0 +1,7 @@
+pub(crate) mod paths;
+pub(crate) mod state_management;
+pub(crate) mod config_loading;
+pub(crate) mod tool_installer;
+pub(crate) mod font_installer;
+pub(crate) mod shell_configurator;
+pub(crate) mod settings_applier;
