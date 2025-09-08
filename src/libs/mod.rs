e0f2a7e2beed6a9b1816e6c86beedbd924a7571e
kodelint <kodelint@gmail.com>
2025-09-08 07:11:27 -0700

    Refactored shell module registry

--- src/libs/mod.rs
+++ src/libs/mod.rs
@@ -1,8 +1,8 @@
-pub(crate) mod paths;
-pub(crate) mod state_management;
 pub(crate) mod config_loading;
-pub(crate) mod tool_installer;
 pub(crate) mod font_installer;
-pub(crate) mod shell_configurator;
+pub(crate) mod paths;
 pub(crate) mod settings_applier;
+pub(crate) mod shell_manager;
+pub(crate) mod state_management;
+pub(crate) mod tool_installer;
 pub(crate) mod utilities;
