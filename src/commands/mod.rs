7945d2652e467edddeb0d1c7aefd407942752508
kodelint <kodelint@gmail.com>
2025-09-11 22:58:17 -0700

    Register the `help` command

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,7 +1,9 @@
 // Register application subcommands.
 // Each module corresponds to a specific `setup-devbox` command-line action.
 
-pub mod version;  // Handles displaying the tool's version and checking for updates.
 pub mod generate; // Manages the creation of default configuration files.
-pub mod now;      // Orchestrates the main setup and installation process.
-pub mod sync;     // Provides functionality to synchronize state with configuration files.
+pub mod help;
+pub mod now; // Orchestrates the main setup and installation process.
+pub mod sync;
+pub mod version; // Handles displaying the tool's version and checking for updates.
+// Provides functionality to synchronize state with configuration files.
